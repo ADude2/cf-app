1b55e4f8803d4458fea0d1f163be64a4befe0795
Alex Di Domenico <alexxdude@gmail.com>
2015-08-12 20:58:25 -0400

    Adds ability for users to update their profiles and styles the Devise registration edit view

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -30,4 +30,43 @@ feature "User authentication" do
 
     expect(page).to have_content("Signed out successfully.")
   end
+
+  scenario "Can edit profile once logged in" do
+    visit root_path
+    
+    click_link "Login"
+    fill_in "Email", with: "lolleroni@example.com"
+    fill_in "Password", with: "helloworld"
+    click_button "Log in"
+
+    visit edit_user_registration_path
+    expect(page).to have_content("Edit User")
+
+    fill_in "Email", with: "lolleroni@example.com"
+    fill_in "Enter Current Password to Save Changes", with: "helloworld"
+    click_button "Update"
+    expect(page).to have_content("Your account has been updated successfully.")
+  end
+
+  scenario "Can delete profile once logged in" do
+    visit root_path
+    
+    click_link "Login"
+    fill_in "Email", with: "lolleroni@example.com"
+    fill_in "Password", with: "helloworld"
+    click_button "Log in"
+
+    visit edit_user_registration_path
+    click_button "Cancel my account"
+
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
+
+  end
+
+  scenario "Must be logged in to update profile" do
+    visit edit_user_registration_path
+    
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+  end
 end
