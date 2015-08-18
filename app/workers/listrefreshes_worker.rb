948c546a57cb7ca2547d78847275cc4f34eab65b
Alex Di Domenico <alexxdude@gmail.com>
2015-08-18 19:30:49 -0400

    Refactors listrefreshes worker to create hashes with the id as the key

--- app/workers/listrefreshes_worker.rb
+++ app/workers/listrefreshes_worker.rb
@@ -2,8 +2,19 @@ class ListrefreshesWorker
   include Sidekiq::Worker
   require 'open-uri'
 
+  def index_url
+    JSON.parse(open("https://map.crossfit.com/getAllAffiliates.php").read)
+  end
+
+  def get_gym_index
+    hash = {}
+    index_url.each do |gym_array|
+      hash[gym_array[3]] = gym_array
+    end
+    hash
+  end
+
   def perform(get_list)
-    get_gym_index = open("https://map.crossfit.com/getAllAffiliates.php").read
     $redis_cache.setex("gymlist", 1.week.seconds, get_gym_index)
   end
 end
