d149f2197a0b547c93d55e4955dc6c0b706f1d6f
Alex Di Domenico <alexxdude@gmail.com>
2015-08-18 23:16:20 -0400

    Completes functionality of the Listrefreshes worker

--- app/workers/listrefreshes_worker.rb
+++ app/workers/listrefreshes_worker.rb
@@ -2,19 +2,30 @@ class ListrefreshesWorker
   include Sidekiq::Worker
   require 'open-uri'
 
-  def index_url
-    JSON.parse(open("https://map.crossfit.com/getAllAffiliates.php").read)
+  def fetch_remote_source
+    remote_source_list = open("https://map.crossfit.com/getAllAffiliates.php").read
+    $redis_cache.setex("remote_source_list", 1.week.seconds, remote_source_list)
   end
 
-  def get_gym_index
-    hash = {}
-    index_url.each do |gym_array|
-      hash[gym_array[3]] = gym_array
+  def remote_source_cached?
+    $redis_cache.exists("remote_source_list")
+  end
+
+  def build_master_list_hash
+    remote_list = JSON.parse($redis_cache.get("remote_source_list"))
+    remote_list.each_with_object({}) do |gym_info, master_list|
+      master_list[gym_info[3]] = gym_info
     end
-    hash
   end
 
   def perform(get_list)
-    $redis_cache.setex("gymlist", 1.week.seconds, get_gym_index)
+    fetch_remote_source unless remote_source_cached?
+    master_list_hash = build_master_list_hash
+    $redis_cache.pipelined do
+      master_list_hash.each do |gym_id, gym_data|
+        $redis_cache.hset("master_list_hash", gym_id, (gym_data).to_json)
+      end
+    end
+    $redis_cache.set("list", master_list_hash.keys)
   end
 end
