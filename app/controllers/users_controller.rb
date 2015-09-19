b249803f982fcb0d52c07c98bdd0321baed9c3d2
Alex Di Domenico <alexxdude@gmail.com>
2015-09-19 12:38:53 -0400

    Fixes error referring to current_user instead of @user in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
-    @my_reviews = current_user.reviews.includes(:gym).paginate(page: params[:page], per_page: 10)
+    @my_reviews = @user.reviews.includes(:gym).paginate(page: params[:page], per_page: 10)
     respond_to do |format|
       format.html # show.html.erb
       format.js
