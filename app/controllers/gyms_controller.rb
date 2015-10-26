22bf55bfea17714d64b6a6537d40ce102355d774
Alex Di Domenico <alexxdude@gmail.com>
2015-10-26 01:09:42 -0400

    Pluralizes term in gym index view and optimizes database query

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -3,6 +3,7 @@ class GymsController < ApplicationController
     if params[:search].present?
       @search = params[:search]
       @gyms = Gym.search_any_word(@search).includes(:reviews).paginate(page: params[:page], per_page: 10)
+      @gym_count = @gyms.count
     else
       flash[:alert] = "Please specify a search term"
       redirect_to root_path
